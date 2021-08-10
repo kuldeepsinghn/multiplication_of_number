e2b0b45867f69e5b6a0d4e6734ff871f66fac815
ksn <kuldeepnaruka934@gmail.com>
2021-08-10 17:18:33 +0530

    In this we are finding the year is leap year or not a leap year

---
+++ leap_year.py
@@ -0,0 +1,17 @@
+def leap_year():
+    year = 2021
+    if (year % 4) == 0:
+        print(year, "is a leap year")
+    elif (year % 100) == 0:
+        print(year, "is a leap year")
+    elif (year % 400) == 0:
+        print(year, "is a leap year")
+    else:
+        print(year, "is not a leap year")
+
+
+
+
+if __name__ == '__main__':
+    leap_year()
+
