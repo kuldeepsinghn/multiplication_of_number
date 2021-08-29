064649a7977aa89453597ee37a8866d34e16b0ac
ksn <kuldeepnaruka934@gmail.com>
2021-08-29 22:28:28 +0530

    find  common number in list

---
+++ common_number.py
@@ -0,0 +1,14 @@
+def common_number():
+    list = {1,2,3,3,4,5,5,5,5,6,66,7,7,7,7,7,7,7,7}
+    n = 19
+    count = 0
+    x= 7
+    for i in list:
+
+            if ( list = x)
+                print( move )
+            else:
+                count:
+
+
+    print("common_number",count)
