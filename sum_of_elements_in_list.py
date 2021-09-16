f01bddfb97ea62ab08eb175b955b7e4052aafcf1
ksn <kuldeepnaruka934@gmail.com>
2021-09-16 21:00:47 +0530

    This is used to find sum of all elements

---
+++ sum_of_elements_in_list.py
@@ -0,0 +1,9 @@
+def sum_of_element():
+    list=[1,2,3,4,6,7,8,9,5]
+    n=9
+    sum = 0
+    sum = (n * (n + 1)) / 2
+    print("sum_of_elements",sum)
+
+if __name__ == '__main__':
+    sum_of_element()
