807b88b9e97562992a1ddeb380d358f2ee8a32de
ksn <kuldeepnaruka934@gmail.com>
2021-09-16 21:08:00 +0530

    It is used to check  the list is empty or not

---
+++ list_empty_or_not.py
@@ -0,0 +1,10 @@
+def empty_or_not():
+    list =[1,2,3]
+
+    if not list:
+        print("list is empty")
+    else:
+        print("list is not empty")
+
+if __name__ == '__main__':
+    empty_or_not()
